6d55b6d0c4dfb4f90d4fcc7514bf6264c13cd5c4
Nate River <nguyenchinhan502@gmail.com>
2022-04-01 12:13:40 +0700

    Update user model

--- src/mongos/user.mongo.js
+++ src/mongos/user.mongo.js
@@ -8,6 +8,11 @@ const UserSchema = new mongoose.Schema({
   address: { type: String, require },
   font_photoPath: { type: String, require },
   back_photoPath: { type: String, require },
+  status: { type: String, default: "" },
+  unnormal_signIn: { type: Number, default: 0 },
+  created_date: { type: Date, require },
+  update: { type: Date },
+  locked_time: { type: Number, default: 0 },
 });
 
 module.exports = mongoose.model("User", UserSchema);
