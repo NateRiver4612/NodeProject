3928fe9078935e10232bba53aae589f65fcaf0e9
Nguyen Chi Nhan <519h0127@student.tdtu.edu.vn>
2022-06-14 14:30:33 +0700

    Update everything

--- libs/mail_transporter.js
+++ libs/mail_transporter.js
@@ -20,8 +20,15 @@ const CLIENT_ID =
   "431679885294-arf4gk2tj6c9tamib36jabq0mbc838tt.apps.googleusercontent.com";
 const CLEINT_SECRET = "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";
 const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+
+// const REFRESH_TOKEN =
+//   "1//04YPs9dKBNuPsCgYIARAAGAQSNwF-L9IrsRINs9dqHv5jUw4X-9ipursYxJZrqLMiUbfKBlPLeEkMlFxYoCCCEHOpCtXNP5dg9ss";
+
 const REFRESH_TOKEN =
-  "1//04YPs9dKBNuPsCgYIARAAGAQSNwF-L9IrsRINs9dqHv5jUw4X-9ipursYxJZrqLMiUbfKBlPLeEkMlFxYoCCCEHOpCtXNP5dg9ss";
+  "1//04a5KCAHBUeFyCgYIARAAGAQSNwF-L9IrO79EaHNbDWW85FSrdwUgZhrvjsesRNmvXa1hkwX3RYRSzROqZ4XNYb0RKHxplo5piG8";
+
+// const REFRESH_TOKEN =
+//   "1//04E1sM5Jd-qJTCgYIARAAGAQSNwF-L9IrBwfB6OvM6z7yDXuMN8T0pvNARNLNSFGtfsYgFopCMnv-SNfA54rMgwAAo-JjNeqbtz8";
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -29,7 +36,10 @@ const oAuth2Client = new google.auth.OAuth2(
   REDIRECT_URI
 );
 
-oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN });
+oAuth2Client.setCredentials({
+  refresh_token: REFRESH_TOKEN,
+  refresh_token_expires_in: 17280000000,
+});
 
 const accessToken = oAuth2Client.getAccessToken();
 
